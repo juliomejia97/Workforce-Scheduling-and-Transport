3644daa529e68f9ffe5ab41e29b63e425ceb2ed0
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-06-03 20:24:51 -0500

    Agent Initialization

--- Worforce Scheduling/src/Airline.java
+++ Worforce Scheduling/src/Airline.java
@@ -1,51 +1,62 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
 
 public class Airline extends Agent{
 
-private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+	private ContainerController container;
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new FileReader(new File("./Agent parameters.csv")));
-		String line = br.readLine();
-		
-		while(line != null) {
-			line = br.readLine();
-			String [] data = line.split(";");
-			String name = "Agente " + data[0].trim();
-			double coorX = Double.parseDouble(data[1].trim());
-			double coorY = Double.parseDouble(data[2].trim());
-			boolean actA = Boolean.parseBoolean(data[3].trim());
-			boolean actB = Boolean.parseBoolean(data[4].trim());
-			boolean actC = Boolean.parseBoolean(data[5].trim());
-			HashMap<String, Boolean> days = new HashMap<String, Boolean>();
-			days.put("Martes", Boolean.parseBoolean(data[6].trim()));
-			days.put("Miercoles", Boolean.parseBoolean(data[7].trim()));
-			days.put("Jueves", Boolean.parseBoolean(data[8].trim()));
-			days.put("Viernes", Boolean.parseBoolean(data[9].trim()));
-			days.put("Sabado", Boolean.parseBoolean(data[10].trim()));
-			days.put("Domingo", Boolean.parseBoolean(data[11].trim()));
-			days.put("Lunes", Boolean.parseBoolean(data[12].trim()));
+	@Override
+	protected void setup() {
+
+		try {
 			
-			System.out.print("Name: " + name);
-			System.out.print(" X: " + coorX);
-			System.out.print(" Y: " + coorY);
-			System.out.print(" A: " + actA);
-			System.out.print(" B: " + actB);
-			System.out.print(" C: " + actC);
-			System.out.println();
+			jade.core.Runtime runtime = jade.core.Runtime.instance();
+			Profile profile = new ProfileImpl();
+			profile.setParameter(Profile.MAIN_HOST, "127.0.0.1");
+			profile.setParameter(Profile.MAIN_PORT, profile.LOCAL_PORT);
+			profile.setParameter(Profile.PLATFORM_ID, "DKJAH");
+			
+			container = runtime.createMainContainer(profile);
+			
+			System.out.println("Airline started...");
+			
+			BufferedReader br;
+			br = new BufferedReader(new FileReader(new File("./Agent parameters.csv")));
+			String line = br.readLine();
+			line = br.readLine();
+			int cont = 1;
 
+			while(line != null) {
+				
+				String name = "Agente ";
+				Object [] params = {line};
+				container.createNewAgent(name + cont, CustomerServiceAgent.class.getName(), params).start();
+				cont++;
+				line = br.readLine();
+				
+			}
+			br.close();
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		} catch (StaleProxyException e) {
+			System.out.println("StaleProxyException: " + e.getMessage());
 		}
-		
-
-		br.close();
 	}
-
 }
---
+++ Worforce Scheduling/src/CustomerServiceAgent.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+
+import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+
+public class CustomerServiceAgent extends Agent{
+	
+	private String name;
+	private double coorX;
+	private double coorY;
+	private boolean actA;
+	private boolean actB;
+	private boolean actC;
+	HashMap<String, Boolean> days = new HashMap<String, Boolean>();
+	
+	@Override
+	protected void setup() {
+		
+		Object[] params = getArguments();
+		String param = params[0].toString();
+		
+		String [] data = param.split(";");
+		this.name = "Agente " + data[0].trim();
+		this.coorX = Double.parseDouble(data[1].trim());
+		this.coorY = Double.parseDouble(data[2].trim());
+		this.actA = Boolean.parseBoolean(data[3].trim());
+		this.actB = Boolean.parseBoolean(data[4].trim());
+		this.actC = Boolean.parseBoolean(data[5].trim());
+		this.days.put("Martes", Boolean.parseBoolean(data[6].trim()));
+		this.days.put("Miercoles", Boolean.parseBoolean(data[7].trim()));
+		this.days.put("Jueves", Boolean.parseBoolean(data[8].trim()));
+		this.days.put("Viernes", Boolean.parseBoolean(data[9].trim()));
+		this.days.put("Sabado", Boolean.parseBoolean(data[10].trim()));
+		this.days.put("Domingo", Boolean.parseBoolean(data[11].trim()));
+		this.days.put("Lunes", Boolean.parseBoolean(data[12].trim()));
+		
+		System.out.println(this.name + " started...");
+		
+	}
+	
+	
+
+}
